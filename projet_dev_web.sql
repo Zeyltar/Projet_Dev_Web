9e4a7990d355098ad2e8734417a753f95ae164aa
Zeyltar <florian.poinsot@ynov.com>
2020-06-11 18:00:40 +0200

    aled

--- projet_dev_web.sql
+++ projet_dev_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 11 juin 2020 à 17:55
+-- Généré le : jeu. 11 juin 2020 à 17:59
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.4
 
@@ -20,6 +20,175 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `projet_dev_web`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ads`
+--
+
+CREATE TABLE `ads` (
+  `AdID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Title` varchar(50) NOT NULL,
+  `Price` float(8,2) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `DatePublication` date NOT NULL,
+  `AdStatus` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `discussions`
+--
+
+CREATE TABLE `discussions` (
+  `DiscussionID` int(11) NOT NULL,
+  `UserID_1` int(11) NOT NULL,
+  `UserID_2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `images`
+--
+
+CREATE TABLE `images` (
+  `ImageID` int(11) NOT NULL,
+  `AdID` int(11) NOT NULL,
+  `ImagePath` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+CREATE TABLE `messages` (
+  `MessageID` int(11) NOT NULL,
+  `DiscussionID` int(11) NOT NULL,
+  `MessageText` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `FirstName` varchar(20) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `UserPassword` char(60) NOT NULL,
+  `PdP_Path` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`AdID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Index pour la table `discussions`
+--
+ALTER TABLE `discussions`
+  ADD PRIMARY KEY (`DiscussionID`),
+  ADD KEY `UserID_1` (`UserID_1`),
+  ADD KEY `UserID_2` (`UserID_2`);
+
+--
+-- Index pour la table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`ImageID`),
+  ADD KEY `AdID` (`AdID`);
+
+--
+-- Index pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`MessageID`),
+  ADD KEY `DiscussionID` (`DiscussionID`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `ads`
+--
+ALTER TABLE `ads`
+  MODIFY `AdID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `discussions`
+--
+ALTER TABLE `discussions`
+  MODIFY `DiscussionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `images`
+--
+ALTER TABLE `images`
+  MODIFY `ImageID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `ads`
+--
+ALTER TABLE `ads`
+  ADD CONSTRAINT `ads_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
+
+--
+-- Contraintes pour la table `discussions`
+--
+ALTER TABLE `discussions`
+  ADD CONSTRAINT `discussions_ibfk_1` FOREIGN KEY (`UserID_1`) REFERENCES `users` (`UserID`),
+  ADD CONSTRAINT `discussions_ibfk_2` FOREIGN KEY (`UserID_2`) REFERENCES `users` (`UserID`);
+
+--
+-- Contraintes pour la table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`AdID`) REFERENCES `ads` (`AdID`);
+
+--
+-- Contraintes pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`DiscussionID`) REFERENCES `discussions` (`DiscussionID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
