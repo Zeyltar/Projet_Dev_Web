228c5d912cb7903e8e9e86706dcd510b9acced6f
Zeyltar <florian.poinsot@ynov.com>
2020-06-16 15:19:37 +0100

    modification sur navbar

--- Projet_Dev_Web/inc/navbar.inc.php
+++ Projet_Dev_Web/inc/navbar.inc.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">Navbar</a>
+  <a class="navbar-brand" href="index.php">ProjetDevWeb</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Rechercher</a>
